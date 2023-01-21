52a814061bcf1fe3acdf9c6efa63f3ad083383bf
Matt Kane <m@mk.gg>
2023-01-21 20:15:51 +0000

    Fix test

--- src/transformers/shopify.test.ts
+++ src/transformers/shopify.test.ts
@@ -1,6 +1,8 @@
+// deno-lint-ignore-file no-explicit-any
 import { assertEquals } from "https://deno.land/std@0.172.0/testing/asserts.ts";
 import { parse, transform } from "./shopify.ts";
 import examples from "./shopify.fixtures.json" assert { type: "json" };
+import { UrlString } from "../types.ts";
 
 const img =
   "https://cdn.shopify.com/s/files/1/2345/6789/products/myimage_medium_crop_top.webp?v=3";
@@ -8,11 +10,12 @@ const img =
 Deno.test("shopify parser", async (t) => {
   for (const { original, ...example } of examples) {
     await t.step(original, () => {
-      const { params, ...parsed } = parse(original) as any;
+      const { params, ...parsed } = parse(original as UrlString) as any;
       // Convert null from JSON into undefined for assertEquals
       const expected = Object.fromEntries(
         Object.entries(example).map(([k, v]) => [k, v ?? undefined]),
       );
+      expected.cdn = "shopify";
       const { crop, size } = params || {};
       assertEquals({ crop, size, ...parsed }, expected);
     });
