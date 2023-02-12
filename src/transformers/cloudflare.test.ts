c7caad09a825df6adc59a846959a98215f584eb2
Nick Randall <nicksrandall@gmail.com>
2023-02-11 20:51:42 -0700

    feat(cloudflare): add support for cloudflare's image cdn

---
+++ src/transformers/cloudflare.test.ts
@@ -0,0 +1,39 @@
+import { assertEquals } from "https://deno.land/std@0.172.0/testing/asserts.ts";
+import { ParsedUrl } from "../types.ts";
+import { CloudflareParams, parse, transform } from "./cloudflare.ts";
+
+const img =
+  "https://assets.example.com/cdn-cgi/image/width=128,height=64,f=auto,background=red/uploads/340210587049918541/group_141-1664915283181.png"
+
+Deno.test("cloudflare parser", () => {
+  const parsed = parse(img);
+  const expected: ParsedUrl<CloudflareParams> = {
+    base: img,
+    cdn: "cloudflare",
+    format: "auto",
+    width: 128,
+    height: 64,
+    params: {
+      host: "assets.example.com",
+      transformations: {
+        background: "red",
+      },
+      path: "uploads/340210587049918541/group_141-1664915283181.png",
+    },
+  };
+  assertEquals(parsed, expected);
+});
+
+Deno.test("cloudfalre transformer", async (t) => {
+  await t.step("transforms a URL", () => {
+    const result = transform({
+      url: img,
+      width: 100,
+      height: 200,
+    });
+    assertEquals(
+      result?.toString(),
+      "https://assets.example.com/cdn-cgi/image/background=red,width=100,height=200,f=auto/uploads/340210587049918541/group_141-1664915283181.png"
+    );
+  });
+});
---
+++ src/transformers/cloudflare.ts
@@ -0,0 +1,111 @@
+import {
+  UrlGenerator,
+  UrlGeneratorOptions,
+  UrlParser,
+  UrlTransformer,
+} from "../types.ts";
+
+// Thanks Colby!
+const cloudflareRegex =
+  /https?:\/\/(?<host>[^\/]+)\/cdn-cgi\/image\/(?<transformations>[^\/]+)\/(?<path>.*)$/g;
+
+const parseTransforms = (transformations: string) =>
+  Object.fromEntries(transformations.split(",").map((t) => t.split("=")));
+
+const formatUrl = (
+  {
+    host,
+    transformations = {},
+    path
+  }: CloudflareParams,
+): string => {
+  console.log('trans', transformations);
+  const transformString = Object.entries(transformations).map(
+    ([key, value]) => `${key}=${value}`,
+  ).join(",");
+
+  const pathSegments = [
+    host,
+    "cdn-cgi",
+    "image",
+    transformString,
+    path
+  ].join("/");
+  return `https://${pathSegments}`;
+};
+
+export interface CloudflareParams {
+  host?: string;
+  transformations: Record<string, string>;
+  path?: string;
+}
+export const parse: UrlParser<CloudflareParams> = (
+  imageUrl,
+) => {
+  const url = new URL(imageUrl);
+  const matches = [...url.toString().matchAll(cloudflareRegex)];
+  if (!matches.length) {
+    throw new Error("Invalid Cloudflare URL");
+  }
+
+  const group = matches[0].groups || {};
+  const {
+    transformations: transformString,
+    ...baseParams
+  } = group;
+
+  const { width, height, f, ...transformations } = parseTransforms(
+    transformString,
+  );
+
+  const base = formatUrl({ ...baseParams, transformations });
+  return {
+    base: url.toString(),
+    width: Number(width) || undefined,
+    height: Number(height) || undefined,
+    format: f,
+    cdn: "cloudflare",
+    params: { ...group, transformations },
+  };
+};
+
+export const generate: UrlGenerator<CloudflareParams> = (
+  { base, width, height, format, params },
+) => {
+  const parsed = parse(base.toString());
+
+  const props: CloudflareParams = {
+    transformations: {},
+    ...parsed.params,
+    ...params,
+  };
+
+  if (width) {
+    props.transformations.width = width?.toString();
+  }
+  if (height) {
+    props.transformations.height = height?.toString();
+  }
+  if (format) {
+    props.transformations.f = format;
+  }
+  return new URL(formatUrl(props));
+};
+
+export const transform: UrlTransformer = (
+  { url: originalUrl, width, height, format = "auto" },
+) => {
+  const parsed = parse(originalUrl);
+  if (!parsed) {
+    throw new Error("Invalid Cloudflare URL");
+  }
+
+  const props: UrlGeneratorOptions<CloudflareParams> = {
+    ...parsed,
+    width,
+    height,
+    format,
+  };
+
+  return generate(props);
+};
