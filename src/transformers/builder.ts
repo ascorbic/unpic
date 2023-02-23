7d6e3f983f9aa96f11cf6d4c23b1385229eb6a52
Steve Sewell <steve@builder.io>
2023-02-23 08:07:02 -0800

    PR feedback and aim to get tests to pass

--- src/transformers/builder.ts
+++ src/transformers/builder.ts
@@ -5,30 +5,36 @@ import {
   setParamIfUndefined,
 } from "../utils.ts";
 
+<<<<<<< HEAD
 export const parse: UrlParser<{ quality?: number }> = (url) => {
+=======
+export const parse: UrlParser<{ fit?: string, quality?: number }> = (url) => {
+>>>>>>> df803a3 (PR feedback and aim to get tests to pass)
   const parsedUrl = new URL(url);
 
   const width = getNumericParam(parsedUrl, "width");
   const height = getNumericParam(parsedUrl, "height");
   const quality = getNumericParam(parsedUrl, "quality");
   const format = parsedUrl.searchParams.get("format") || undefined;
+  const fit = parsedUrl.searchParams.get("fit") || undefined;
   parsedUrl.search = "";
   return {
     width,
     height,
     format,
     base: parsedUrl.toString(),
-    params: { quality },
+    params: { quality, fit },
     cdn: "builder",
   };
 };
 
 export const transform: UrlTransformer = (
-  { url: originalUrl, width, height, format },
+  { url: originalUrl, width, height, format, fit },
 ) => {
   const url = new URL(originalUrl);
   setParamIfDefined(url, "width", width, true, true);
   setParamIfDefined(url, "height", height, true, true);
   setParamIfDefined(url, "format", format);
+  setParamIfDefined(url, "fit", fit, "cover");
   return url;
 };
