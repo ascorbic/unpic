7475b58a258878faf2c9f15c094de477294992b1
ref-thomasledoux1 <74651094+ref-thomasledoux1@users.noreply.github.com>
2023-03-06 21:09:55 +0100

    feat: add kontent.ai CDN (#19)
    * Adding kontent.ai CDN
    
    * Update README.md
    
    * Fix tests
    
    * Fix tests + fix fit for kontent.ai
    
    * Change scale to crop as default fit
    
    ---------
    
    Co-authored-by: Thomas Ledoux <thomas_ledoux@epam.com>
    Co-authored-by: Matt Kane <m@mk.gg>

---
+++ src/transformers/kontentai.ts
@@ -0,0 +1,39 @@
+import { UrlParser, UrlTransformer } from "../types.ts";
+import {
+  getNumericParam,
+  setParamIfDefined,
+  setParamIfUndefined
+} from "../utils.ts";
+
+export const parse: UrlParser<{ fit?: string }> = (url) => {
+  const parsedUrl = new URL(url);
+
+  const fit = parsedUrl.searchParams.get("fit") || undefined;
+  const width = getNumericParam(parsedUrl, "w");
+  const height = getNumericParam(parsedUrl, "h");
+  const quality = getNumericParam(parsedUrl, "q");
+  const format = parsedUrl.searchParams.get("fm") || undefined;
+
+  parsedUrl.search = "";
+  return {
+    width,
+    height,
+    format,
+    base: parsedUrl.toString(),
+    params: { fit, quality },
+    cdn: "kontent.ai",
+  };
+};
+
+export const transform: UrlTransformer = (
+  { url: originalUrl, width, height, format },
+) => {
+  const url = new URL(originalUrl);
+  setParamIfDefined(url, "w", width, true, true);
+  setParamIfDefined(url, "h", height, true, true);
+  setParamIfDefined(url, "fm", format, true);
+  if (width || height) {
+    setParamIfUndefined(url, "fit", "crop");
+  }
+  return url;
+};
