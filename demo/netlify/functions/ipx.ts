dfbb6fa826dcfbcd25879d7bc5627b00b274f304
ascorbic <ascorbic@gmail.com>
2023-10-29 17:10:32 +0000

    chore: use ipx 2

--- demo/netlify/functions/ipx.ts
+++ demo/netlify/functions/ipx.ts
@@ -1,16 +1,28 @@
-import { createIPXHandler } from "@netlify/ipx";
+import {
+  createIPX,
+  createIPXWebServer,
+  ipxFSStorage,
+  ipxHttpStorage,
+} from "ipx";
 
-const handle = createIPXHandler({
-  domains: [
-    "placekitten.com",
-  ],
-  basePath: "/_ipx/",
-  responseHeaders: {
-    "Strict-Transport-Security": "max-age=31536000",
-    "X-Test": "foobar",
-  },
+const ipx = createIPX({
+  httpStorage: ipxHttpStorage({
+    domains: [
+      "placekitten.com",
+    ],
+  }),
+  storage: ipxFSStorage({ dir: "./public/_ipx" }),
 });
 
-export const handler = (event, context) => {
-  return handle(event, context);
+const server = createIPXWebServer(ipx);
+
+export default function handler(request: Request, context) {
+  const url = new URL(request.url);
+  url.pathname = url.pathname.replace(/^\/_ipx/, "");
+
+  return server(new Request(url, request), context);
+}
+
+export const config = {
+  path: "/_ipx/*",
 };
