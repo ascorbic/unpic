dd1a92d926a2850ac07e02cb7637f779c6a3ee6e
ascorbic <ascorbic@gmail.com>
2023-10-31 19:22:05 +0000

    fix: correct param handling and add tests

---
+++ src/transformers/netlify.test.ts
@@ -0,0 +1,154 @@
+import { assertEquals } from "https://deno.land/std@0.172.0/testing/asserts.ts";
+import { parse, transform } from "./netlify.ts";
+
+const img = "https://example.netlify.app/.netlify/images?url=/cappadocia.jpg";
+
+const remoteImage = "https://example.org/static/moose.png";
+
+Deno.test("netlify", async (t) => {
+  await t.step("should parse a URL", () => {
+    const result = parse(
+      "https://example.netlify.app/.netlify/images?url=/cappadocia.jpg&w=200&h=300&fit=crop&q=80&fm=webp",
+    );
+    assertEquals(
+      result.width,
+      200,
+    );
+    assertEquals(
+      result.height,
+      300,
+    );
+    assertEquals(
+      result.format,
+      "webp",
+    );
+    assertEquals(
+      result.base,
+      "/cappadocia.jpg",
+    );
+  });
+  await t.step("should format a URL", () => {
+    const result = transform({
+      url: img,
+      width: 200,
+      height: 100,
+    });
+    assertEquals(
+      result?.toString(),
+      "https://example.netlify.app/.netlify/images?w=200&h=100&fit=crop&url=%2Fcappadocia.jpg",
+    );
+  });
+
+  await t.step("should format a remote URL", () => {
+    const result = transform({
+      url:
+        "/.netlify/images?w=800&h=600&&url=https%3A%2F%2Fexample.org%2Fstatic%2Fbuffalo.png",
+      width: 200,
+      height: 100,
+    });
+    assertEquals(
+      result?.toString(),
+      "/.netlify/images?w=200&h=100&fit=crop&url=https%3A%2F%2Fexample.org%2Fstatic%2Fbuffalo.png",
+    );
+  });
+
+  await t.step("should not set height if not provided", () => {
+    const result = transform({ url: img, width: 200 });
+    assertEquals(
+      result?.toString(),
+      "https://example.netlify.app/.netlify/images?w=200&fit=crop&url=%2Fcappadocia.jpg",
+    );
+  });
+
+  await t.step("should delete height if not set", () => {
+    const url = new URL(img);
+    url.searchParams.set("h", "100");
+    const result = transform({ url, width: 200 });
+    assertEquals(
+      result?.toString(),
+      "https://example.netlify.app/.netlify/images?w=200&fit=crop&url=%2Fcappadocia.jpg",
+    );
+  });
+
+  await t.step("should round non-integer dimensions", () => {
+    const result = transform({
+      url: img,
+      width: 200.6,
+      height: 100.2,
+    });
+    assertEquals(
+      result?.toString(),
+      "https://example.netlify.app/.netlify/images?w=201&h=100&fit=crop&url=%2Fcappadocia.jpg",
+    );
+  });
+
+  await t.step("should transform a remote image", () => {
+    const result = transform({
+      url: remoteImage,
+      width: 100,
+      height: 200,
+      format: "webp",
+    });
+    assertEquals(
+      result?.toString(),
+      "/.netlify/images?w=100&h=200&fm=webp&fit=crop&url=https%3A%2F%2Fexample.org%2Fstatic%2Fmoose.png",
+    );
+  });
+
+  await t.step("should transform a local image", () => {
+    const result = transform({
+      url: "/static/moose.png",
+      width: 100,
+      height: 200,
+      format: "webp",
+    });
+    assertEquals(
+      result?.toString(),
+      "/.netlify/images?w=100&h=200&fm=webp&fit=crop&url=%2Fstatic%2Fmoose.png",
+    );
+  });
+
+  await t.step("should transform a remote image with a relative base", () => {
+    const result = transform({
+      url: remoteImage,
+      width: 100,
+      height: 200,
+      format: "webp",
+      cdnOptions: {
+        netlify: {
+          site: "https://petsofnetlify.com",
+        },
+      },
+    });
+    assertEquals(
+      result?.toString(),
+      "https://petsofnetlify.com/.netlify/images?w=100&h=200&fm=webp&fit=crop&url=https%3A%2F%2Fexample.org%2Fstatic%2Fmoose.png",
+    );
+  });
+
+  await t.step("should rename aliased params", () => {
+    const result = transform({
+      url:
+        "/.netlify/images?width=800&h=600&quality=10&url=https%3A%2F%2Fexample.org%2Fstatic%2Fbuffalo.png",
+      width: 200,
+      height: 100,
+    });
+    assertEquals(
+      result?.toString(),
+      "/.netlify/images?q=10&w=200&h=100&fit=crop&url=https%3A%2F%2Fexample.org%2Fstatic%2Fbuffalo.png",
+    );
+  });
+
+  await t.step("should preserve other params", () => {
+    const result = transform({
+      url:
+        "/.netlify/images?width=800&h=600&quality=10&fit=scale&url=https%3A%2F%2Fexample.org%2Fstatic%2Fbuffalo.png",
+      width: 200,
+      height: 100,
+    });
+    assertEquals(
+      result?.toString(),
+      "/.netlify/images?fit=scale&q=10&w=200&h=100&url=https%3A%2F%2Fexample.org%2Fstatic%2Fbuffalo.png",
+    );
+  });
+});
--- src/transformers/netlify.ts
+++ src/transformers/netlify.ts
@@ -6,24 +6,45 @@ import {
   toUrl,
 } from "../utils.ts";
 
+const skippedParams = new Set([
+  "w",
+  "h",
+  "q",
+  "fm",
+  "url",
+  "width",
+  "height",
+  "quality",
+]);
 export const parse: UrlParser = (
   url,
 ) => {
   const parsed = toUrl(url);
-  const width = Number(parsed.searchParams.get("w")) || undefined;
-  const height = Number(parsed.searchParams.get("h")) || undefined;
-  const quality = Number(parsed.searchParams.get("q")) || undefined;
+  const width =
+    Number(parsed.searchParams.get("w") ?? parsed.searchParams.get("width")) ??
+      undefined;
+  const height =
+    Number(parsed.searchParams.get("h") ?? parsed.searchParams.get("height")) ??
+      undefined;
+  const quality = Number(
+    parsed.searchParams.get("q") ?? parsed.searchParams.get("quality"),
+  ) || undefined;
   const format = parsed.searchParams.get("fm") || undefined;
-  const params: Record<string, string> = {};
+  const base = parsed.searchParams.get("url") || "";
+  const params: Record<string, string | undefined | number> = {
+    quality,
+  };
   parsed.searchParams.forEach((value, key) => {
+    if (skippedParams.has(key)) {
+      return;
+    }
     params[key] = value;
   });
   parsed.search = "";
   return {
-    base: parsed.toString(),
+    base,
     width,
     height,
-    quality,
     format,
     params,
     cdn: "netlify",
@@ -33,13 +54,19 @@ export const parse: UrlParser = (
 export interface NetlifyParams {
   /** If set, use this site as the base for absolute image URLs. Otherwise, generate relative URLs */
   site?: string;
+  fit?: string;
+  quality?: number;
 }
 export const generate: UrlGenerator<NetlifyParams> = (
-  { base, width, height, format, params },
+  { base, width, height, format, params: { site, quality, ...params } = {} },
 ) => {
-  const url = toUrl("/.netlify/images", params?.site);
-  setParamIfDefined(url, "w", width, false, true);
-  setParamIfDefined(url, "h", height, false, true);
+  const url = toUrl("/.netlify/images", site);
+  Object.entries(params).forEach(([key, value]) =>
+    setParamIfDefined(url, key, value)
+  );
+  setParamIfDefined(url, "q", quality, true, true);
+  setParamIfDefined(url, "w", width, true, true);
+  setParamIfDefined(url, "h", height, true, true);
   setParamIfDefined(url, "fm", format);
   setParamIfUndefined(url, "fit", "crop");
   url.searchParams.set("url", base.toString());
@@ -53,12 +80,14 @@ export const transform: UrlTransformer = (
 
   // If this is a Netlify image URL, we'll manipulate it rather than using it as the source image
   if (url.pathname.startsWith("/.netlify/images")) {
-    const { params, ...parsedOptions } = parse(url);
+    const { params, base, format } = parse(url);
+    console.log({ params });
     return generate({
-      ...parsedOptions,
-      ...params,
+      base,
+      format,
       ...options,
       params: {
+        ...params,
         // If hostname is "n", we're dealing with a relative URL, so we don't need to set the site param
         site: url.hostname === "n" ? undefined : url.origin,
       },
