d06651ed2d74e0c6123855d7129e1abab15c023a
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2024-02-07 23:19:24 +0700

    fix(imagekit): set format param to "f" (#116)
    * fix(imagekit): set format param to "f"
    
    Imagekit format param was incorrectly named "fm". This PR ensures the param is correctly named "f".
    
    * chore: format
    
    ---------
    
    Co-authored-by: Matt Kane <matt.kane@netlify.com>

--- src/transformers/imagekit.test.ts
+++ src/transformers/imagekit.test.ts
@@ -1,8 +1,8 @@
 import { assertEquals } from "https://deno.land/std@0.172.0/testing/asserts.ts";
-import { transform, parse} from "./imagekit.ts";
+import { parse, transform } from "./imagekit.ts";
 
 const img =
-  "https://ik.imagekit.io/subman/v2/asset-3d/icon-standard.png?tr=w-500,h-300,fm-png,q-80";
+  "https://ik.imagekit.io/subman/v2/asset-3d/icon-standard.png?tr=w-500,h-300,f-png,q-80";
 
 Deno.test("imagekit", async (t) => {
   await t.step("should format a URL", () => {
@@ -13,7 +13,7 @@ Deno.test("imagekit", async (t) => {
     });
     assertEquals(
       result?.toString(),
-      "https://ik.imagekit.io/subman/v2/asset-3d/icon-standard.png?tr=w-200%2Ch-100%2Cfm-png%2Cq-80",
+      "https://ik.imagekit.io/subman/v2/asset-3d/icon-standard.png?tr=w-200%2Ch-100%2Cf-png%2Cq-80",
     );
   });
 
@@ -21,15 +21,15 @@ Deno.test("imagekit", async (t) => {
     const result = transform({ url: img, width: 200 });
     assertEquals(
       result?.toString(),
-      "https://ik.imagekit.io/subman/v2/asset-3d/icon-standard.png?tr=w-200%2Cfm-png%2Cq-80",
+      "https://ik.imagekit.io/subman/v2/asset-3d/icon-standard.png?tr=w-200%2Cf-png%2Cq-80",
     );
   });
 
-  await t.step("should delete height if not set", () => {  
+  await t.step("should delete height if not set", () => {
     const result = transform({ url: img, width: 200 });
     assertEquals(
       result?.toString(),
-      "https://ik.imagekit.io/subman/v2/asset-3d/icon-standard.png?tr=w-200%2Cfm-png%2Cq-80",
+      "https://ik.imagekit.io/subman/v2/asset-3d/icon-standard.png?tr=w-200%2Cf-png%2Cq-80",
     );
   });
 
@@ -41,39 +41,41 @@ Deno.test("imagekit", async (t) => {
     });
     assertEquals(
       result?.toString(),
-      "https://ik.imagekit.io/subman/v2/asset-3d/icon-standard.png?tr=w-201%2Ch-100%2Cfm-png%2Cq-80",
+      "https://ik.imagekit.io/subman/v2/asset-3d/icon-standard.png?tr=w-201%2Ch-100%2Cf-png%2Cq-80",
     );
   });
 
-  await t.step("should set fm-auto if no format is provided", () => {
-    
+  await t.step("should set f-auto if no format is provided", () => {
     const imgWithoutFormat =
-  "https://ik.imagekit.io/subman/v2/asset-3d/icon-standard.png?tr=w-500,h-300,q-80";
+      "https://ik.imagekit.io/subman/v2/asset-3d/icon-standard.png?tr=w-500,h-300,q-80";
 
     const result = transform({ url: imgWithoutFormat, width: 200 });
     assertEquals(
       result?.toString(),
-      "https://ik.imagekit.io/subman/v2/asset-3d/icon-standard.png?tr=w-200%2Cq-80%2Cfm-auto",
+      "https://ik.imagekit.io/subman/v2/asset-3d/icon-standard.png?tr=w-200%2Cq-80%2Cf-auto",
     );
   });
 
-  await t.step("should not set fm-auto if format is provided and use provided format", () => {
-    const result = transform({ url: img, width: 200, format: "jpg" });
-    assertEquals(
-      result?.toString(),
-      "https://ik.imagekit.io/subman/v2/asset-3d/icon-standard.png?tr=w-200%2Cfm-jpg%2Cq-80",
-    );
-  });
-
+  await t.step(
+    "should not set f-auto if format is provided and use provided format",
+    () => {
+      const result = transform({ url: img, width: 200, format: "jpg" });
+      assertEquals(
+        result?.toString(),
+        "https://ik.imagekit.io/subman/v2/asset-3d/icon-standard.png?tr=w-200%2Cf-jpg%2Cq-80",
+      );
+    },
+  );
 
   await t.step("should parse url", () => {
     const result = parse(img);
 
-    
-    assertEquals(result.base, "https://ik.imagekit.io/subman/v2/asset-3d/icon-standard.png");
+    assertEquals(
+      result.base,
+      "https://ik.imagekit.io/subman/v2/asset-3d/icon-standard.png",
+    );
     assertEquals(result.width, 500);
     assertEquals(result.height, 300);
     assertEquals(result.format, "png");
   });
-
 });
