704181f2a8d69b3edcd5498e61e389fe78b06393
Andrei Bobkov <slider7259@gmail.com>
2024-03-12 15:58:21 +0700

    fix(supabase): Image format

--- src/transformers/supabase.ts
+++ src/transformers/supabase.ts
@@ -6,6 +6,8 @@ import {
 } from "../types.ts";
 import { roundIfNumeric, toUrl } from "../utils.ts";
 
+const ALLOWED_FORMATS = ["origin"];
+
 const STORAGE_URL_PREFIX = "/storage/v1/object/public/";
 const RENDER_URL_PREFIX = "/storage/v1/render/image/public/";
 
@@ -94,7 +96,7 @@ export const generate: UrlGenerator<SupabaseParams> = (
 
   if (height) searchParams.set("height", roundIfNumeric(height).toString());
 
-  if (format) searchParams.set("format", format);
+  if (format && ALLOWED_FORMATS.includes(format)) searchParams.set("format", format);
 
   if (params?.quality) {
     searchParams.set("quality", roundIfNumeric(params.quality).toString());
