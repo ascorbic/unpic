29e722cd9787b43520433d2e879b3280a58c38d7
Julian Benegas <julianbenegas99@gmail.com>
2024-11-19 18:56:09 -0300

    add fit query param

--- src/transformers/basehub.test.ts
+++ src/transformers/basehub.test.ts
@@ -24,6 +24,7 @@ Deno.test("basehub", async (t) => {
 			"https://assets.basehub.com/fa068a12/qZeFxPJWNB7UQdwUzjX3e/features-streamlined-team-communication-real-time-messaging-(light-mode)3x.jpg?w=200",
 		);
 	});
+
 	await t.step("should delete height if not set", () => {
 		const url = new URL(img);
 		url.searchParams.set("h", "100");
@@ -44,74 +45,6 @@ Deno.test("basehub", async (t) => {
 		assertEquals(
 			result?.toString(),
 			"https://assets.basehub.com/fa068a12/qZeFxPJWNB7UQdwUzjX3e/features-streamlined-team-communication-real-time-messaging-(light-mode)3x.jpg?w=201&h=100&format=webp",
-		);
-	});
-
-	await t.step("should not set fit=fill if another value exists", () => {
-		const url = new URL(img);
-		url.searchParams.set("fit", "crop");
-		const result = transform({ url, width: 200 });
-		assertEquals(
-			result?.toString(),
-			"https://assets.basehub.com/fa068a12/qZeFxPJWNB7UQdwUzjX3e/features-streamlined-team-communication-real-time-messaging-(light-mode)3x.jpg?fit=crop&w=200",
-		);
-	});
-
-	await t.step("should bracket width if > 4000", () => {
-		const result = transform({
-			url: img,
-			width: 5000,
-		});
-		assertEquals(
-			result?.toString(),
-			"https://assets.basehub.com/fa068a12/qZeFxPJWNB7UQdwUzjX3e/features-streamlined-team-communication-real-time-messaging-(light-mode)3x.jpg?w=4000",
-		);
-	});
-
-	await t.step("should adjust height proportionally if width > 4000", () => {
-		const result = transform({
-			url: img,
-			width: 5000,
-			height: 2000,
-		});
-		assertEquals(
-			result?.toString(),
-			"https://assets.basehub.com/fa068a12/qZeFxPJWNB7UQdwUzjX3e/features-streamlined-team-communication-real-time-messaging-(light-mode)3x.jpg?w=4000&h=1600",
-		);
-	});
-
-	await t.step("should bracket height if > 4000", () => {
-		const result = transform({
-			url: img,
-			height: 5000,
-		});
-		assertEquals(
-			result?.toString(),
-			"https://assets.basehub.com/fa068a12/qZeFxPJWNB7UQdwUzjX3e/features-streamlined-team-communication-real-time-messaging-(light-mode)3x.jpg?h=4000",
-		);
-	});
-
-	await t.step("should adjust width proportionally if height > 4000", () => {
-		const result = transform({
-			url: img,
-			width: 2000,
-			height: 5000,
-		});
-		assertEquals(
-			result?.toString(),
-			"https://assets.basehub.com/fa068a12/qZeFxPJWNB7UQdwUzjX3e/features-streamlined-team-communication-real-time-messaging-(light-mode)3x.jpg?w=1600&h=4000",
-		);
-	});
-
-	await t.step("it should adjust width and height if both are > 4000", () => {
-		const result = transform({
-			url: img,
-			width: 6000,
-			height: 4500,
-		});
-		assertEquals(
-			result?.toString(),
-			"https://assets.basehub.com/fa068a12/qZeFxPJWNB7UQdwUzjX3e/features-streamlined-team-communication-real-time-messaging-(light-mode)3x.jpg?w=4000&h=3000",
 		);
 	});
 });
--- src/transformers/basehub.ts
+++ src/transformers/basehub.ts
@@ -9,7 +9,7 @@ import {
 export const parse: UrlParser<{ fit?: string }> = (url) => {
 	const parsedUrl = toUrl(url);
 
-	const fit = parsedUrl.searchParams.get("fit") || undefined;
+	const fit = parsedUrl.searchParams.get("fit") || 'cover';
 	const width = getNumericParam(parsedUrl, "w");
 	const height = getNumericParam(parsedUrl, "h");
 	const quality = getNumericParam(parsedUrl, "q");
@@ -29,22 +29,14 @@ export const transform: UrlTransformer = (
 	{ url: originalUrl, width, height, format },
 ) => {
 	const url = toUrl(originalUrl);
-	if (width && width > 4000) {
-		if (height) {
-			height = Math.round(height * 4000 / width);
-		}
-		width = 4000;
-	}
-
-	if (height && height > 4000) {
-		if (width) {
-			width = Math.round(width * 4000 / height);
-		}
-		height = 4000;
-	}
 
 	setParamIfDefined(url, "w", width, true, true);
 	setParamIfDefined(url, "h", height, true, true);
 	setParamIfDefined(url, "format", format);
+
+	if (width && height) {
+		setParamIfUndefined(url, "fit", "cover");
+	}
+
 	return url;
 };
